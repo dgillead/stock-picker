de97ac31a888f06dd217b41e68e31b5668e23a7c
Darnell Gillead <dlgillead@gmail.com>
2017-06-20 11:19:20 -0500

    added getLowestValue and getHighestValue functions

---
+++ apple-stock-picker.js
@@ -0,0 +1,18 @@
+var stockPrices = [10, 7, 5, 8, 11, 9];
+
+function stockPicks(stockPrices) {
+  let lowestValue = getLowestValue(stockPicks);
+  let highestValue = getHighestValue(stockPicks);
+  // console.log(lowestValue);
+  // console.log(highestValue);
+};
+
+function getLowestValue(stockPicks) {
+  return Math.min.apply(null, stockPrices);
+};
+
+function getHighestValue(stockPicks) {
+  return Math.max.apply(null, stockPrices);
+};
+
+stockPicks(stockPrices);
