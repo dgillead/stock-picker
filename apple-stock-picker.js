866cdd95c1f9a12bec20074871999fcea796e179
Darnell Gillead <dlgillead@gmail.com>
2017-06-20 11:27:54 -0500

    moved getting max and min numbers to separate functions

--- apple-stock-picker.js
+++ apple-stock-picker.js
@@ -3,6 +3,7 @@ var stockPrices = [10, 7, 5, 8, 11, 9];
 function stockPicks(stockPrices) {
   let lowestValue = getLowestValue(stockPicks);
   let highestValue = getHighestValue(stockPicks);
+
   // console.log(lowestValue);
   // console.log(highestValue);
 };
